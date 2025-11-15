aef812e22e5cb5b5a9f9b0d0db9249bff4ac0760
7Bssk7 <arhip.fin3@gmail.com>
2025-11-14 18:23:48 -0800

    Update, just testing

--- 210-lab-32.cpp
+++ 210-lab-32.cpp
@@ -8,5 +8,14 @@ int main(){
 
     deque<Car> list;
 
+    for(int i = 0; i < 5; ++i){
+        Car temp_car;
+        list.push_back(temp_car);
+    }
+
+    for(int i = 0; i < list.size(); ++i){
+        list.at(i).print();
+    }
+
     return 0;
 }
