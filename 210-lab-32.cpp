de4f4864f74d5eb196be41deeb6087ec2aac3c86
7Bssk7 <arhip.fin3@gmail.com>
2025-11-14 18:42:20 -0800

    Update

--- 210-lab-32.cpp
+++ 210-lab-32.cpp
@@ -11,22 +11,26 @@ bool chance(int );
 int main(){
 
     deque<Car> list(SIZE);
+    int count = 1;
 
     cout << "Imitial queue:" << endl;
     for(int i = 0; i < list.size(); ++i){
         list.at(i).print();
     }
 
-    /*
     while(!(list.empty())){
+        cout << "Time: " << count << "Operation: ";
         if(chance(55)){
+            cout << "Car paid: ";
 
         }
         else if(chance(45)){
-
+            Car temp_car;
+            cout << "Joined lane: " << temp_car.print();
         }
+
     }
-    */
+    
 
     return 0;
 }
